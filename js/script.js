258dfb08ed4259f69fd47272e4e0c4545e09b803
Krzysiek <kgrygiel@gmail.com>
2019-07-08 22:53:53 +0200

    quote style fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&fi
 var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    fetch(prefix + quoteUrl, { cache: "no-store" })
+    fetch(quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
